1412c8f8a56b10ae2456e42b5abd481e7cf9ff3d
solid <987585547@qq.com>
2025-04-08 15:30:44 +0800

    category

--- SolidShop.Vue/SolidShop-Vue/src/apis/category.js
+++ SolidShop.Vue/SolidShop-Vue/src/apis/category.js
@@ -1,10 +1,19 @@
 import httpInstance from "@/utils/http";
 
-export function getCategoryAPI(id){
+export function getCategoryAPI(id) {
     return httpInstance({
         url: '/good/findCategory',
         params: {
             id
         }
+    })
+}
+
+
+export function getSubCategoryAPI(data) {
+    return httpInstance({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
     })
 }
